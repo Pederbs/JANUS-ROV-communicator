d09f32f3944d73ff97f89964084de8abb07b67d4
HaIvor <45287121+HaIvor@users.noreply.github.com>
2023-06-05 14:37:10 +0200

    tatt vekk tull

--- src/nodecomx_cpp_py/nodecomx_cpp_py/consensus.py
+++ src/nodecomx_cpp_py/nodecomx_cpp_py/consensus.py
@@ -7,39 +7,22 @@ import matplotlib.pyplot as plt
 import sympy as sym
 import sys 
 
-"""
-The values we are actually optimizing: 
-
-N = optimizable - Subcarriers 
-M = optimizable - Modulation order
-m = optimizable - Symbols per packet
-"""
-
-
 # Number of out neighbors. Since this simulation is just using two nodes, they each have one neighbor.
 out_neigh = 1 
-out_neigh2 = 1
 
 # How exact one wants before the simulation stops. A lower tolerance value => more exact
 tolerance = 1e-3
 
-# We set a very low epsilon value first iteration to not get a big jump in the beginning
-epsilon = 0.00001
-
 # Max iterations (if it never converges)
 max_iter = 50
 
-
-# -----------------Initialization node i--------------------
-
-# Initial estimate for node i for the global optimization. Arbitrary values.
+# Initial values
 ox1 = 100
 ox2 = 10
 
-flag_reception, flag_update, flag_transmission, flag_update2, flag_reception2, flag_transmission2 = 0, 0, 0, 0, 0, 0
+flag_reception, flag_transmission, flag_reception2, flag_transmission2 = 0, 0, 0, 0
 flag_transmission = 1
 
-# Used for when plotting N, M and m.
 #node i
 ox_list = np.array(ox1)
 
@@ -68,14 +51,12 @@ while i < max_iter:
         # These sigmas are broadcasted to the neighbors (just setting them global in the simulation)
         sigma_ox1 = sigma_ox1 + ox1
 
-        flag_reception, flag_update, flag_transmission, flag_update2, flag_reception2, flag_transmission2 = 0, 0, 0, 0, 0, 0
+        flag_reception, flag_transmission, flag_reception2, flag_transmission2 = 0, 0, 0, 0
         flag_reception2 = 1
         
 # ------Data Reception node i------
 
     if flag_reception == 1:
-
-        # Should get these values from node j: transmitter_node_ID, sigmaj_y and sigmaj_z 
 
         ox1 = ox1 + sigma_ox2 - rho_ox1
 
@@ -83,7 +64,7 @@ while i < max_iter:
 
         rho_ox1 = sigma_ox2
 
-        flag_reception, flag_update, flag_transmission, flag_update2, flag_reception2, flag_transmission2 = 0, 0, 0, 0, 0, 0
+        flag_reception, flag_transmission, flag_reception2, flag_transmission2 = 0, 0, 0, 0
         flag_transmission = 1
     
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -94,19 +75,18 @@ while i < max_iter:
         transmitter_node_ID = "j" # This is what will be transmitted (but not really in this simulation)
         
         # Push sum consensus
-        ox2 = (1/(out_neigh2 + 1))*ox2
+        ox2 = (1/(out_neigh + 1))*ox2
         
         # These sigmas are broadcasted to the neighbors (just setting them global in the simulation)
         sigma_ox2 = sigma_ox2 + ox2
 
-        flag_reception, flag_update, flag_transmission, flag_update2, flag_reception2, flag_transmission2 = 0, 0, 0, 0, 0, 0
+        flag_reception, flag_transmission, flag_reception2, flag_transmission2 = 0, 0, 0, 0
         flag_reception = 1
 
     # ------Data Reception node j------
 
     if flag_reception2 == 1:
 
-        # Should get values from node j: transmitter_node_ID, sigmaj_y and sigmaj_z
         if i == 1:
             ox2 = (1/(out_neigh + 1))*ox2
         
@@ -118,7 +98,10 @@ while i < max_iter:
 
         i += 1 
 
-        flag_reception, flag_update, flag_transmission, flag_update2, flag_reception2, flag_transmission2 = 0, 0, 0, 0, 0, 0
+        # Should get values from node j: transmitter_node_ID, sigmaj_y and sigmaj_z
+        
+
+        flag_reception, flag_transmission, flag_reception2, flag_transmission2 = 0, 0, 0, 0
         flag_transmission2 = 1
 
 
