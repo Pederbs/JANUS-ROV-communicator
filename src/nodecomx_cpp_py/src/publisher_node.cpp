ed46282c36530b2b4d91ad00efc64e8a4e59bc56
Peder <pederbsa@stud.ntnu.no>
2023-06-06 12:13:46 +0200

    cleanup

--- src/nodecomx_cpp_py/src/publisher_node.cpp
+++ src/nodecomx_cpp_py/src/publisher_node.cpp
@@ -17,8 +17,11 @@ public:
     ComPubNode() : Node("minimal_publisher"), count_(0)
     {
         publisher_ = this->create_publisher<std_msgs::msg::String>("modemTopic", 10);
-        timer_ = this->create_wall_timer(
-            500ms, std::bind(&ComPubNode::timer_callback, this));
+        timer_ = this->create_wall_timer
+        (
+            500ms, 
+            std::bind(&ComPubNode::timer_callback, this)
+        );
         RCLCPP_INFO(this->get_logger(), "EvoLogistics Publisher Node has been started.");
     }
 
