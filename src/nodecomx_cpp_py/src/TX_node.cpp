51a208cbdaf6be07a1ee652f9d913c545841d315
Peder <pederbsa@stud.ntnu.no>
2023-06-06 17:08:25 +0200

    cleanup

--- src/nodecomx_cpp_py/src/TX_node.cpp
+++ src/nodecomx_cpp_py/src/TX_node.cpp
@@ -21,8 +21,8 @@
 
 
 //Constructor parameters for Evo_janusXsdm.h
-std::string JANUSPATH = "src/nodecomx_cpp_py/include/lib/janus-c-3.0.5/bin/";
-std::string SDMPATH = "src/nodecomx_cpp_py/include/lib/sdmsh/";
+std::string JANUS_PATH = "src/nodecomx_cpp_py/include/lib/janus-c-3.0.5/bin/";
+std::string SDM_PATH = "src/nodecomx_cpp_py/include/lib/sdmsh/";
 std::string IP ="192.168.0.189";
 int JANUS_RX_PORT = 9920;
 int JANUS_TX_PORT = 9914;
@@ -34,7 +34,7 @@ public:
     ComSubNode() : Node("evo_communicator")
     {
         //Constructing a connection object
-        Evo_janusXsdm::connection modem(IP,JANUSPATH,SDMPATH, JANUS_RX_PORT,JANUS_TX_PORT,STREAMFS); 
+        Evo_janusXsdm::connection modem(IP,JANUS_PATH,SDM_PATH, JANUS_RX_PORT,JANUS_TX_PORT,STREAMFS); 
         //Configures modem and sets preamble
         modem.sdmConfigAir();
         std::this_thread::sleep_for(500ms);
@@ -51,14 +51,15 @@ public:
         RCLCPP_INFO(this->get_logger(), "EvoLogistics Subscriber Node has been started.");
     }
 private:
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
+
     void callbackSensorValues(const std_msgs::msg::String::SharedPtr msg)
     {
         RCLCPP_INFO(this->get_logger(), "Heard: '%s'", msg->data.c_str());
         //Constructing a connection object
-        Evo_janusXsdm::connection modem(IP,JANUSPATH,SDMPATH, JANUS_RX_PORT,JANUS_TX_PORT,STREAMFS); 
+        Evo_janusXsdm::connection modem(IP,JANUS_PATH,SDM_PATH, JANUS_RX_PORT,JANUS_TX_PORT,STREAMFS); 
         modem.startTX(msg->data.c_str());
     }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
 };
 
 
